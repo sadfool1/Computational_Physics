d4774d474e55c1edce5ea14863a4377ea6351394
James <takw14@gmail.com>
2020-05-05 21:38:03 +0800

    Some fixes

--- Final Project/test.py
+++ Final Project/test.py
@@ -6,7 +6,7 @@ from random import randint
 import math
 from tkinter import *
 import tkinter as tk
-import matplotlib
+import matplotlib 
 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.figure import Figure
@@ -14,45 +14,18 @@ from matplotlib import style
 
 matplotlib.use("TkAgg")
 
-"""
-class window():
-    
-    def __init__(self):
-        self.master = Tk()
-        self.master.grid()
-        
-        self.master.title("Lorenz Simulation")
-        self.master.geometry = ("500x500")
-        self.master.resizable = (False, False)
-
-        self.master.graphframe = LabelFrame(master = self.master, text = "Phase Space")
-        self.master.graphframe.grid()
-        
-        self.master.label1 = Label(self.master.graphframe, text="Graph", height = 3, width = 15).grid(row=5, column=0, columnspan=10)
-        
-        self.slider = Scale(self.master, from_=0, to=42, orient=HORIZONTAL)
-        self.slider.grid(row = 0, column = 0)
-        
-        self.master.label2 = Label(self.master, text="r value", height = 1, width = 12).grid(row=10, column=10, columnspan=1)
-        
-        self.master.mainloop()
-"""      
-
 class LorenzAttractorRungeKutta(tk.Frame):
     DT            = 1e-3     # Differential interval
     STEP          = 100000   # Time step count
     
-    #print (X_0, Y_0, Z_0) #Show randomised initial conditions
 
     
     def __init__(self):
-        global r_entry
-        global sigma_entry
-        global b_entry
+        global user_r_entry
+        global user_sigma_entry
+        global user_b_entry
         
         super(LorenzAttractorRungeKutta, self).__init__()
-        #self.widgets={}
-        #self.grid(column=0,row=0)
         
         self.X_0, self.Y_0, self.Z_0 = 0,0,0
         
@@ -65,31 +38,53 @@ class LorenzAttractorRungeKutta(tk.Frame):
         self.res = [[], [], []]
 
         self.root.graphframe = LabelFrame(master = self.root, text = "Phase Space")
-        self.root.label1 = Label(self.root.graphframe, text="Graph", height = 3, width = 15).grid(row=5, column=0, columnspan=10)
+        self.root.label1 = Label(self.root.graphframe, text="Graph", height = 3, width = 15)
+        self.root.label1.grid(row=0, column=0, columnspan=10)
         
         #self.root.graphframe = LabelFrame(master = self.root, text = "Phase Space")
         #self.root.label1 = Label(self.root.graphframe, text="Graph", height = 3, width = 15).grid(row=5, column=0, columnspan=10)
-        
-        r_entry = Entry(self.root).grid(row = 1, column = 0)
+        user_r_entry = IntVar()
+        r_entry = Entry(self.root, textvariable = user_r_entry).grid(row = 1, column = 0)
         self.root.r_label = Label(self.root, text="r value", height = 1, width = 12).grid(row=2, column=0, columnspan=1)
                 
-        sigma_entry = Entry(self.root).grid(row = 1, column = 1)
+        user_sigma_entry = IntVar()
+        sigma_entry = Entry(self.root, textvariable = user_sigma_entry).grid(row = 1, column = 1)
         self.root.sigma_label = Label(self.root, text="sigma value", height = 1, width = 12).grid(row=2, column=1, columnspan=1)
         
-        b_entry = Entry(self.root).grid(row = 1, column = 2)
+        user_b_entry = IntVar()
+        b_entry = Entry(self.root, textvariable = user_b_entry).grid(row = 1, column = 2)
         self.root.b_label = Label(self.root, text="b value", height = 1, width = 12).grid(row=2, column=2, columnspan=1)
         
-        self.plot_button = Button (self.root, command = self.plot, height = 4, width = 2).grid(row = 3, column = 1)
+        self.plot_button = Button (self.root, command = self.click1, height = 2, width = 8, text = "Run").grid(row = 3, column = 1)
         
-        self.root.mainloop()
-
+        self.mainloop()
+        
+        
+    def click1(self):
+        global r_info
+        global sigma_info
+        global b_info
+        
+        r_info = user_r_entry.get()
+        sigma_info = user_sigma_entry.get()
+        b_info = user_b_entry.get()
+        
+        print (r_info, sigma_info, b_info)
+        
     def plot(self):
+        global user_r_entry
+        global user_sigma_entry
+        global user_b_entry
+        global r_info
+        global sigma_info
+        global b_info
+        
+        print (r_info,sigma_info,b_info)
         
         tk.Frame.__init__(self, self.root)
         
         try:
             xyz = [self.X_0, self.Y_0, self.Z_0]
-            print (xyz)
             for _ in range(self.STEP):
                 k_0 = self.__lorenz(xyz)
                 k_1 = self.__lorenz([x + k * self.DT / 2 for x, k in zip(xyz, k_0)])
@@ -100,7 +95,7 @@ class LorenzAttractorRungeKutta(tk.Frame):
                             * self.DT / 6.0
                     self.res[i].append(xyz[i])
                     
-            print (self.res[1])
+            #print (self.res[1])
             
             f = Figure(figsize=(10,10), dpi=100)
             a = f.add_subplot(111)
@@ -114,17 +109,14 @@ class LorenzAttractorRungeKutta(tk.Frame):
         except Exception as e:
             raise
 
-    def __lorenz(self, xyz, p=10, r=24.4
-                 , b=8/3.0):
-        global r_entry
-        global sigma_entry
-        global b_entry
+    def __lorenz(self, xyz):
+        global r_info
+        global sigma_info
+        global b_info
         
-        
-        print (sigma_entry,r_entry,b_entry)
-        p = sigma_entry
-        r = r_entry
-        b = b_entry
+        p = sigma_info
+        r = r_info
+        b = b_info
 
         try:
             return [
